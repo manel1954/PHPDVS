6ce4746a0549d179cfa023718aa8a00063ad1d8c
manel1954 <55663831+manel1954@users.noreply.github.com>
2026-05-25 11:39:57 +0200

    1

--- html/mmdvm.php
+++ html/mmdvm.php
@@ -843,7 +843,7 @@ button.btn-header { font-family: var(--font-mono); }
 </a>
 <span style="color:amber;font-size:1.9rem;font-family: Bebas Neue, sans-serif;">PANEL SISTEMAS DIGITALES</span>
 <span style="color:#ff8c00;font-size:1.9rem;font-family: Bebas Neue, sans-serif;">PARA RADIOAFICIONADOS</span>
-<span style="color:cyan;font-size:1.9rem;font-family: Bebas Neue, sans-serif;">PHPDVS</span>
+<span style="color:#4169f1;font-size:1.9rem;font-family: Bebas Neue, sans-serif;">PHPDVS</span>
 
 </div>
 <div class="ctrl-header-btns">
