2611dfe0fe75a9c461da162f63312e8f1f601ee1
manel1954 <55663831+manel1954@users.noreply.github.com>
2026-05-25 02:22:20 +0200

    Update mmdvm.php

--- html/mmdvm.php
+++ html/mmdvm.php
@@ -842,7 +842,7 @@ button.btn-header { font-family: var(--font-mono); }
 <a href="?action=backup-configs" class="btn-header amber"> 💾 Hacer copia de seguridad </a>
 <button onclick="openRestore()" class="btn-header cyan"> ✅  Restaurar copia de seguridad </button>
 <div class="dropdown-wrap" id="dropActualizaciones">
-  <button class="btn-header cyan">⬇ Actualizaciones ▾</button>
+  <button class="btn-header cyan">🧿 Actualizaciones ▾</button>
   <div class="dropdown-menu-custom">
     <button class="dropdown-item-custom" onclick="runUpdate('imagen')">🖼 Actualizar Imagen</button>
     <button class="dropdown-item-custom" onclick="runUpdate('ids')">📋 Actualizar IDs</button>
@@ -850,8 +850,8 @@ button.btn-header { font-family: var(--font-mono); }
     <button class="dropdown-item-custom" onclick="window.location.href='dstar_json_converter.php'">📡 Actualizar Reflectores D-STAR</button>
   </div>
 </div>
-<button class="btn-header cyan" onclick="xtTtydOpen()">⌨ Terminal</button>
-<a href="extra.php" class="btn-header amber">☰ Menu Extra</a>
+<button class="btn-header cyan" onclick="xtTtydOpen()">🖥️Terminal</button>
+<a href="extra.php" class="btn-header amber">🚀 Menu Extra</a>
 <button id="btnReboot" class="btn-header red" onclick="rebootPi()">⏻ Reiniciar Pi</button>
 </div>
 </header>
