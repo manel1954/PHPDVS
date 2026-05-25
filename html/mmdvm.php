cc9599c55363ad43c83bbe2111e9eaa62a078c81
manel1954 <55663831+manel1954@users.noreply.github.com>
2026-05-25 10:45:03 +0200

    1

--- html/mmdvm.php
+++ html/mmdvm.php
@@ -856,8 +856,8 @@ button.btn-header { font-family: var(--font-mono); }
   </div>
 </div>
 <button class="btn-header cyan" onclick="xtTtydOpen()">🖥️Terminal</button>
-<a href="extra.php" class="btn-header amber">🚀 Menu Extra</a>
-<button id="btnReboot" class="btn-header red" onclick="rebootPi()">⏻ Reiniciar Pi</button>
+<a href="extra.php" class="btn-header amber">⚙️Menu Extra</a>
+<button id="btnReboot" class="btn-header red" onclick="rebootPi()">🔌Reiniciar Pi</button>
 </div>
 </header>
 <main class="ctrl-body">
