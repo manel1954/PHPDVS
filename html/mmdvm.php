2a6647639349de0a1c4447e06e9d9179dd664400
manel1954 <55663831+manel1954@users.noreply.github.com>
2026-06-04 22:52:42 +0200

    1

--- html/mmdvm.php
+++ html/mmdvm.php
@@ -897,7 +897,7 @@ button.btn-header { font-family: var(--font-mono); }
 <div class="dropdown-wrap" id="dropBridge">
   <button class="btn btn-light btn-sm" style="color:#000;border-color:#b06090;" onclick="toggleDropdownBridge(event)">🔗 Bridges ▾</button>
   <div class="dropdown-menu-custom">
-    <a class="dropdown-item-custom" style="font-size:.80rem; text-decoration: none;" href="mmdvmdmr2ysf.php">🔗 DMR2YSF</a>
+    <a class="dropdown-item-custom" style="font-size:.80rem; text-decoration: none;" href="dmr2ysf.php">🔗 DMR2YSF</a>
     <a class="dropdown-item-custom" style="font-size:.80rem; text-decoration: none;" href="ysf2dmr.php">🔗 YSF2DMR</a>
   </div>
 </div>
