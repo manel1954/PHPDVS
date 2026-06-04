e1266fd52337755edb4f31620ca37e4966f3843a
manel1954 <55663831+manel1954@users.noreply.github.com>
2026-06-05 00:47:41 +0200

    Update transcoding.php

--- html/transcoding.php
+++ html/transcoding.php
@@ -70,7 +70,7 @@ body {
     position: relative;
     overflow: hidden;
     transition: transform .15s, box-shadow .15s;
-    height: 220px;
+    height: 180px;
 }
 .bridge-card:hover {
     transform: translateY(-3px);
